3d88ff6ee41f9dbccc53d7a53993da406280120f
Zerokoska <vasulhavruliv1.c@gmail.com>
2024-11-11 11:44:11 +0100

    update status model

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Person;
 
 class Status extends Model
 {
-    //
+    public function people(){
+        return $this->hasMany(Person::class);
+    }
 }
