ae01c235c8f5e0490d58f00a2e6ff646d46e59ea
erikvlcak <erikvlcak1@gmail.com>
2024-11-11 11:42:26 +0100

    create person relationships

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Alias;
+use App\Models\Image;
+use App\Models\Status;
 
 class Person extends Model
 {
-    //
+    public function aliases()
+    {
+        return $this->hasMany(Alias::class);
+    }
+
+    public function image()
+    {
+        return $this->belongsTo(Image::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(Status::class);
+    }
 }
