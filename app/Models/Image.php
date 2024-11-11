a3d21bddbd26a54e9a9f5e384aac7b6f6d741c9c
Zerokoska <vasulhavruliv1.c@gmail.com>
2024-11-11 11:46:24 +0100

    patched Image model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Person;
 
 class Image extends Model
 {
-    public function Person(){
-        return $this->hasOne(Image::class);
+    public function person(){
+        return $this->hasOne(Person::class);
     }
 }
